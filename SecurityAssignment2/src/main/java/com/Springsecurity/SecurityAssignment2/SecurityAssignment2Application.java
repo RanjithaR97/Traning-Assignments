bd79a00c3b927c95cef7431b44ab823e589eb7fa
RanjithaR97 <101782896+RanjithaR97@users.noreply.github.com>
2022-07-21 14:18:50 +0530

    Add files via upload
    SpringsecurityAss2

---
+++ SecurityAssignment2/src/main/java/com/Springsecurity/SecurityAssignment2/SecurityAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.Springsecurity.SecurityAssignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAssignment2Application.class, args);
+	}
+
+}
---
+++ SecurityAssignment2/src/main/java/com/Springsecurity/SecurityAssignment2/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.Springsecurity.SecurityAssignment2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "HI ";
+	}
+	
+	@GetMapping("/user")
+	public String user() {
+		return "Welcome User";
+	}
+
+	@GetMapping("/admin")
+	public String admin() {
+		return "Welcome  Admin";
+	}
+
+}
