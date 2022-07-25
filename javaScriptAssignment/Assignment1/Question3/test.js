1a4001cecd529c15d7c58555cb19a91d0284bbfa
RanjithaR97 <101782896+RanjithaR97@users.noreply.github.com>
2022-07-25 09:53:24 +0530

    Add files via upload
    Javascript assignments

---
+++ javaScriptAssignment/Assignment1/Question3/test.js
@@ -0,0 +1,5 @@
+var n = prompt("Enter the Numbe");
+if (n % 2 === 0)
+    document.getElementById("demo").innerHTML = "Even Number";
+else
+    document.getElementById("demo").innerHTML = "Odd Number";
